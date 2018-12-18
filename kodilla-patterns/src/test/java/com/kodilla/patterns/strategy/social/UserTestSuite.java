15681755aba1ccd44133ac580c940316214c9310
SlawomirJablonski <forest19@interia.pl>
2018-12-18 19:39:28 +0100

    added ex15.3

---
+++ kodilla-patterns/src/test/java/com/kodilla/patterns/strategy/social/UserTestSuite.java
@@ -0,0 +1,35 @@
+package com.kodilla.patterns.strategy.social;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class UserTestSuite {
+    @Test
+    public void testDefaultSharingStrategies(){
+        //Given
+        User danka = new Millenials("hotDanka");
+        User jerzy = new YGeneration("jurekKameralnie");
+        User wojt = new ZGeneration("wójtPoImprzie");
+
+        //When
+        String dankaLog = danka.sharePost();
+        String jerzyLog = jerzy.sharePost();
+        String wojtLog = wojt.sharePost();
+
+        //Then
+        Assert.assertEquals("hotDanka had insert new post on Facebook",dankaLog);
+        Assert.assertEquals("jurekKameralnie had insert new post on Twitter",jerzyLog);
+        Assert.assertEquals("wójtPoImprzie had insert new post on Snapchat",wojtLog);
+    }
+
+    @Test
+    public void testIndividualSharingStrategy(){
+        //Given
+        User jerzy = new YGeneration("jurekKameralnie");
+        //When
+        jerzy.setSocialPublisher(new SnapchatPublisher());
+        String jerzyLog = jerzy.sharePost();
+        //Then
+        Assert.assertEquals("jurekKameralnie had insert new post on Snapchat",jerzyLog);
+    }
+}
