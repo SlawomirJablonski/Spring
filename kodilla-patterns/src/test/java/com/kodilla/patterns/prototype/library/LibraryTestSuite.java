52ee256a805148a0bec60f7c21583bbc9423a20c
SlawomirJablonski <forest19@interia.pl>
2019-01-02 20:18:38 +0100

    ex15.2 (added private field in Book, separate tests for shallow and deep copy in LibraryTestSuite)

--- kodilla-patterns/src/test/java/com/kodilla/patterns/prototype/library/LibraryTestSuite.java
+++ kodilla-patterns/src/test/java/com/kodilla/patterns/prototype/library/LibraryTestSuite.java
@@ -20,6 +20,26 @@ public class LibraryTestSuite {
         library.getBooks().add(b2);
         library.getBooks().add(b3);
 
+        //When
+        library.getBooks().remove(b3);
+
+        //Then
+        Assert.assertEquals(2,library.getBooks().size());
+    }
+
+    @Test
+    public void testShallowCopy() {
+        //Given
+        Book b1 = new Book("Japoński nie gryzie!","Opracowanie zbiorowe", LocalDate.of(2014,11,14));
+        Book b2 = new Book("Regulamin tłoczni win","Irving John", LocalDate.of(1985,05,10));
+        Book b3 = new Book("Honor germańskiego bobra","Roman Bratny", LocalDate.of(1985,11,02));
+
+        Library library = new Library("Christmas gifts /origin/");
+
+        library.getBooks().add(b1);
+        library.getBooks().add(b2);
+        library.getBooks().add(b3);
+
         //making a shallow copy of object library
         Library shallowCopyLibrary = null;
         try{
@@ -28,6 +48,27 @@ public class LibraryTestSuite {
         }catch (CloneNotSupportedException e){
             System.out.println(e);
         }
+
+        //When
+        library.getBooks().remove(b3);
+
+        //Then
+        Assert.assertEquals(2,shallowCopyLibrary.getBooks().size());
+
+    }
+
+    @Test
+    public void testDeepCopy() {
+        //Given
+        Book b1 = new Book("Japoński nie gryzie!","Opracowanie zbiorowe", LocalDate.of(2014,11,14));
+        Book b2 = new Book("Regulamin tłoczni win","Irving John", LocalDate.of(1985,05,10));
+        Book b3 = new Book("Honor germańskiego bobra","Roman Bratny", LocalDate.of(1985,11,02));
+
+        Library library = new Library("Christmas gifts /origin/");
+
+        library.getBooks().add(b1);
+        library.getBooks().add(b2);
+        library.getBooks().add(b3);
 
         //making a deep copy of object library
         Library deepCopyLibrary = null;
@@ -42,8 +83,6 @@ public class LibraryTestSuite {
         library.getBooks().remove(b3);
 
         //Then
-        Assert.assertEquals(2,library.getBooks().size());
-        Assert.assertEquals(2,shallowCopyLibrary.getBooks().size());
         Assert.assertEquals(3,deepCopyLibrary.getBooks().size());
     }
 }
